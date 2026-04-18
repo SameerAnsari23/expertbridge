dbb8893c4fdc7e0eb78981502ffca191070cd03e
SameerAnsari23 <sameeransariscience@gmail.com>
2026-04-19 00:01:54 +0530

    Backend

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,12 @@ const PORT = process.env.PORT || 5000;
 const MONGODB_URI =
   process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/surveyDB";
 
-app.use(cors());
+app.use(
+  cors({
+    origin: true,
+    credentials: true,
+  })
+);
 app.use(express.json());
 app.use("/api", surveyRoutes);
 
